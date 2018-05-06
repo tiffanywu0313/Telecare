09920c0bccafbdb153a58a2ef2bc0a90d25b4dc1
Kelvin Chan <chan.kelv@gmail.com>
2018-05-05 20:14:36 -0700

    session created

--- hackhealth_android/app/src/main/java/hackhealth2018/myapplication/views/chatSession/ChatFragment.java
+++ hackhealth_android/app/src/main/java/hackhealth2018/myapplication/views/chatSession/ChatFragment.java
@@ -2,19 +2,29 @@ package hackhealth2018.myapplication.views.chatSession;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
 import com.opentok.android.OpentokError;
 import com.opentok.android.Session;
 import com.opentok.android.Stream;
 
+import java.io.IOException;
+
 import hackhealth2018.myapplication.R;
+import hackhealth2018.myapplication.model.SessionResponse;
 import hackhealth2018.myapplication.util.Strings;
 import hackhealth2018.myapplication.views.BaseFragment;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ChatFragment extends BaseFragment implements Session.SessionListener  {
     private static final String TAG = ChatFragment.class.getSimpleName();
@@ -55,6 +65,46 @@ public class ChatFragment extends BaseFragment implements Session.SessionListene
     }
 
     public void onChatStart() {
+        OkHttpClient client = new OkHttpClient();
+
+        String role = isDoctor ? "doctor" : "patient";
+        Request request = new Request.Builder()
+                .url("https://hackhealth2018.herokuapp.com/" + "room/" +"12894/" + role)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                Log.d(TAG, "Session start failed: " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                String responseBody = response.body().string();
+                if (response.isSuccessful()) {
+                    SessionResponse sessionResponse = new Gson().fromJson(responseBody, SessionResponse.class);
+                    if (sessionResponse != null)
+                        sessionStart(sessionResponse);
+                    else
+                        Log.d(TAG, "Session response error");
+                    response.close();
+                }
+            }
+        });
+        mSession = new Session.Builder(mContext, API_KEY, SESSION_ID).build();
+        mSession.setSessionListener(this);
+        mSession.connect(TOKEN);
+    }
+
+    private void sessionStart(SessionResponse sessionData) {
+        API_KEY = sessionData.getApiKey();
+        SESSION_ID = sessionData.getSessionId();
+        TOKEN = sessionData.getToken();
+
+        Log.i(TAG, "API_KEY: " + API_KEY);
+        Log.i(TAG, "SESSION_ID: " + SESSION_ID);
+        Log.i(TAG, "TOKEN: " + TOKEN);
+
         mSession = new Session.Builder(mContext, API_KEY, SESSION_ID).build();
         mSession.setSessionListener(this);
         mSession.connect(TOKEN);
